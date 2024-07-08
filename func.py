91787ce1a7e4e210e4aa310ae058bc4d02c54426
meghalee_d <meghalee.dey@oracle.com>
2024-07-08 15:17:00 +0000

    2nd commit

--- func.py
+++ func.py
@@ -7,7 +7,7 @@ import numpy as np
 import time
 from fdk import response
 from urllib.parse import urlparse, parse_qs
-
+#from langchain_community.embeddings import CohereEmbeddings
 # Load Documents
 def load_documents(file_path):
     with open(file_path, "r") as f:
@@ -17,7 +17,7 @@ def load_documents(file_path):
 # Initialize Cohere client
 cohere_api_key = "TX8xfSGQm7btpYjQBrf3qYHyo7M9gAXtGrp2kJtT"
 co = cohere.Client(cohere_api_key)
-
+api_url = "https"
 # Function to embed texts
 def embed_texts(texts):
     response = co.embed(
@@ -35,43 +35,30 @@ def retrieve_relevant_documents(query, document_texts, document_embeddings, top_
 
 # Function to generate API URL
 def generate_api(template, question, retrieved_text):
-    prompt = template.format(api_docs=retrieved_text, question=question)
-    response = co.generate(
-        prompt=prompt,
-        model='command',
-        max_tokens=1000,
-        return_likelihoods="NONE",
-        temperature=0,
-        p=1,
-        truncate="END"
-    )
-    generated = response.generations[0].text.strip()
-    return generated
+    for doc in retrieved_text:
+        prompt = template.format(api_docs=doc, question=question)
+        response = co.generate(
+            prompt=prompt,
+            model='command',
+            max_tokens=1000,
+            return_likelihoods="None",
+            temperature=0,
+            p=1,
+            truncate="END"
+        )
+        generated = response
+        time.sleep(10)
+        generated = response.generations[0].text.strip()
+        return generated  # Assuming you want the first 115 characters
 
-def handler(ctx, data: io.BytesIO = None):
+def handler(ctx, data: io.BytesIO=None):
     print("Entering Python Hello World handler", flush=True)
     name = "World"
-    query = ""
     try:
         body = json.loads(data.getvalue())
-        query = body.get("query", "Which API can I use to get the enrollments data")
-        template = body.get("template", '''
-        API url:  https://example.com/hcmRestApi/resources/11.13.18.05/emps?q=FirstName=Derek;LastName=Kam&fields=HireDate
-
-        API url: https://example.com/hcmRestApi/resources/11.13.18.05/emps?q=FirstName=Derek;LastName=Kam&fields=HireDate&onlyData=True
-
-        You are given the below API Documentation:
-        {api_docs}
-        Using this documentation, generate the full API url to call for answering the user question.
-        You should build the API url in order to get a response that is as short as possible, while still getting the necessary information to answer the question.
-        Pay attention to deliberately exclude any unnecessary pieces of data in the API call.
-        Do not include data not in the documentation.
-
-        Question: {question}
-        API url:
-        ''')
-
+        name = body.get("name")
         file_path = "hcm_api_doc.txt"
+        query = "Which API can I use to get the enrollments data"
 
         # Load documents from file
         extracted_text = load_documents(file_path)
@@ -86,16 +73,34 @@ def handler(ctx, data: io.BytesIO = None):
         retrieved_text = "\n".join(retrieved_docs)
 
         # Generate API URL using retrieved documents and query
+        template = '''
+        API url:  https://example.com/hcmRestApi/resources/11.13.18.05/emps?q=FirstName=Derek;LastName=Kam&fields=HireDate
+
+        API url: https://example.com/hcmRestApi/resources/11.13.18.05/emps?q=FirstName=Derek;LastName=Kam&fields=HireDate&onlyData=True
+
+        You are given the below API Documentation:
+        {api_docs}
+        Using this documentation, generate the full API url to call for answering the user question.
+        You should build the API url in order to get a response that is as short as possible, while still getting the necessary information to answer the question.
+        Pay attention to deliberately exclude any unnecessary pieces of data in the API call.
+        Do not include data not in the documentation.
+
+        Question:{question}
+        API url:
+        '''
+
         api_url = generate_api(template, query, retrieved_text)
         parsed_url = urlparse(api_url)
-        query = parse_qs(parsed_url.query)
-        print("Value of api_url = ", api_url, flush=True)
+        query = parsed_url.query
+        print(query)
+        print("Vale of api_url = ", api_url, flush=True)
     except (Exception, ValueError) as ex:
         print(str(ex), flush=True)
 
-    print("Value of name = ", name, flush=True)
+    print("Vale of name = ", name, flush=True)
     print("Exiting Python Hello World handler", flush=True)
     return response.Response(
-        ctx, response_data=json.dumps({"api_url": query}),
+        ctx, response_data=json.dumps(
+            {"api_url": query}),
         headers={"Content-Type": "application/json"}
-    )
+        )
